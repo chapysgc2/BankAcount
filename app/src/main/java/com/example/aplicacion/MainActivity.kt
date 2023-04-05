b43dd9f16e2b10c6b2eb6840fd108930c5740948
chapysgc2 <hazelo235@gmail.com>
2023-04-04 23:14:02 -0600

    Se agrego tarjeta de credito

--- app/src/main/java/com/example/aplicacion/MainActivity.kt
+++ app/src/main/java/com/example/aplicacion/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.aplicacion
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val loginButton: Button = findViewById(R.id.login)
+        loginButton.setOnClickListener {
+            val intent = Intent(this, Home::class.java)
+            startActivity(intent)
+        }
     }
 }
